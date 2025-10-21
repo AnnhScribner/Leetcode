c85a2aea6cf8f803f08dddbeaff4958411c75eb0
Anna Marques <annh.scribner@gmail.com>
2025-10-21 06:00:13 -0700

    Adding new tests for Two sum exercise and correcting the return

--- easy/1/TwoSum.py
+++ easy/1/TwoSum.py
@@ -18,12 +18,15 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     list_to_be_returned.append(i)
                     list_to_be_returned.append(j)
-       
-        return list_to_be_returned
+                    return list_to_be_returned
+        return []
     
 
-sol = Solution()    
+s = Solution()
 
-print(sol.twoSum([2, 7, 11, 15], 9)) # target: 9, output: [0, 1]
-print(sol.twoSum([3, 2, 4], 6)) # target: 6, output: [1, 2]
-print(sol.twoSum([3, 3], 6)) # target: 6, output: [0, 1]
+print(s.twoSum([2, 7, 11, 15], 9))   # Expected: [0, 1]
+print(s.twoSum([3, 2, 4], 6))        # Expected: [1, 2]
+print(s.twoSum([3, 3], 6))           # Expected: [0, 1]
+print(s.twoSum([1, 5, 3, 7, 9], 10)) # Expected: [0, 4]
+print(s.twoSum([1, 2, 3], 7))        # Expected: []  (no valid pair)
+print(s.twoSum([0, 4, 3, 0], 0))     # Expected: [0, 3]
