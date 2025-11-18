e4a9fc5ec638b876a12fdf42056acc91e3c05320
Anna Marques <annh.scribner@gmail.com>
2025-11-17 20:15:57 -0800

    adding merge alternately leetcode exercise

---
+++ easy/1768/MergeStrings.py
@@ -0,0 +1,36 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # Create an empty string that will store the final merged result
+        new_word = ""
+
+        # We loop using an index i from 0 up to the length of the longer word.
+        for i in range(max(len(word1), len(word2))):
+        
+            # If i is still a valid index for word1,
+            if i < len(word1):
+                # keeping adding
+                new_word += word1[i]
+                
+            # If i is still a valid index for word2,
+            if i < len(word2):
+                # keep adding
+                new_word += word2[i]
+               
+        # After the loop finishes, new_word contains all characters from both
+        # strings, merged in alternating order as much as possible.
+        return new_word
+        
+        
+s = Solution()
+print(s.mergeAlternately(['a', 'b', 'c', 'd'], ['p', 'q', 'r']))
+print(s.mergeAlternately(['p', 'q', 'r'], ['a', 'b', 'c', 'd']))
+
+
+
+
+
+
+
+# arr = ['1', '2', '3']
+# arr.insert(2, '9')
+# print(arr)
